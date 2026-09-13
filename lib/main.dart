74db947fbec8b8ad69c792ef5da792690ee2d691
Subodha Sandawaruni <pdssandawaruni@students.nsbm.ac.lk>
2026-09-13 09:16:33 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 // Note: Adjust the import path if your project name is different
-import 'package:datamodel_listdata/model/book_details.dart'; 
+import 'package:datamodel_listdata/model/book_details.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: BookListScreen(),
-  ));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BookListScreen(),
+    ),
+  );
 }
 
 class BookListScreen extends StatefulWidget {
@@ -19,34 +21,36 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
-  
   // TODO 4: Create a List of BookDetails objects and add at least 3 books to it.
   // Example: BookDetails(title: "THE FROZEN RIVER", author: "Ariel Lawhon")
   List<BookDetails> books = [
-    
+    BookDetails(title: "THE FROZEN RIVER", author: "Ariel Lawhon"),
+    BookDetails(title: "THE GREAT GATSBY", author: "F. Scott Fitzgerald"),
+    BookDetails(title: "TO KILL A MOCKINGBIRD", author: "Harper Lee"),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Book App"),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text("Book App"), centerTitle: true),
       body: SafeArea(
         child: Column(
           children: [
             Expanded(
               child: ListView.builder(
                 // TODO 5: Set the itemCount to the total number of items in your 'books' list
-                itemCount: 0, 
-                
+                itemCount: books.length,
                 itemBuilder: (context, index) {
                   // TODO 6: Return a ListTile widget to display the book data
                   // - Set the 'title' parameter using a Text widget: Text(books[index].title)
                   // - Set the 'trailing' parameter using a Text widget for the author
-                  // - Add an 'onTap' parameter with an anonymous function that prints the title to the console
-                  return Container(); 
+                  return ListTile(
+                    title: Text(books[index].title),
+                    trailing: Text(books[index].author),
+                    onTap: () {
+                      print(books[index].title);
+                    },
+                  );
                 },
               ),
             ),
@@ -55,4 +59,5 @@ class _BookListScreenState extends State<BookListScreen> {
       ),
     );
   }
+  // - Add an 'onTap' parameter with an anonymous function that prints the title to the console
 }
--- lib/model/book_details.dart
+++ lib/model/book_details.dart
@@ -1,8 +1,9 @@
 // TODO 1: Create a class named BookDetails
 class BookDetails {
   // TODO 2: Declare two String variables: 'title' and 'author'
-  
-  
+  String title;
+  String author;
+
   // TODO 3: Create a constructor that requires 'title' and 'author' to be passed in
-  
+  BookDetails({required this.title, required this.author});
 }
